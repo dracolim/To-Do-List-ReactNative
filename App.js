670b0b9ba571904839e720a2eb0f8f3989804052
CherylLim <85498185+dracolim@users.noreply.github.com>
2022-10-04 21:49:09 +0800

    foundation of the app

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Today's Task */}
+      <View style = {styles.tasksWrapper}>
+        <Text style = {styles.sectionTitle}>
+          Today's Task
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
